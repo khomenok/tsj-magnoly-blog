acb86090a6c25d986d6d43214b23e79a606985e4
Vladimir Khomenok <khomenokvladimir@gmail.com>
2023-05-28 12:58:27 +0200

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   output: "server",
   adapter: vercel()
 });
