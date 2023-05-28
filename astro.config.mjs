debacb4b52a81ead8ab7ad37c9b68929510ddeee
Vladimir Khomenok <khomenokvladimir@gmail.com>
2023-05-28 13:29:31 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import mdx from "@astrojs/mdx";
 import vercel from "@astrojs/vercel/serverless";
 
 import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://тсжмагнолия.рф',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), robotsTxt()],
   output: "server",
   adapter: vercel()
 });
