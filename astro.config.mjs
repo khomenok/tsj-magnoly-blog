964121b1f533b9ab032e06cb7de46810ea4bbeed
Vladimir Khomenok <khomenokvladimir@gmail.com>
2023-05-28 13:04:41 +0200

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://тсжмагнолия.рф',
   integrations: [mdx(), sitemap()],
   output: "server",
   adapter: vercel()
